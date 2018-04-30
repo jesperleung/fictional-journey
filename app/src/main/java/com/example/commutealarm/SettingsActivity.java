a248397405c8cd83007f8679f6363ef0cc11f233
Timothy Lai <timothyandrelai@gmail.com>
2018-04-29 20:19:42 -0400

    Connected seekbar and textedit input to one another.

--- app/src/main/java/com/example/commutealarm/SettingsActivity.java
+++ app/src/main/java/com/example/commutealarm/SettingsActivity.java
@@ -1,7 +1,13 @@
 package com.example.commutealarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +15,55 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        final SeekBar seekBarDistance = (SeekBar) findViewById(R.id.seekBarDistance); // initiate the Seekbar
+        seekBarDistance.setProgress(1000); // set Seekbar to either default value, or whatever value was selected before
+
+        final TextView distanceValue = (TextView)findViewById(R.id.distanceValue); //initialize the TextView
+        final EditText desiredDistance = (EditText)findViewById(R.id.desiredDistance);
+
+
+        desiredDistance.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "1500")});
+
+        seekBarDistance.setMax(1500); // 1500 maximum value for the Seek bar
+        seekBarDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                distanceValue.setText(String.valueOf(progress));
+                desiredDistance.setText(String.valueOf(progress));
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        desiredDistance.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+//                int desiredIntValue = Integer.parseInt(desiredDistance.toString());
+//                String desiredStringVal = desiredDistance.getText().toString();
+//                seekBarDistance.setProgress(desiredIntValue);
+
+                //Update Seekbar value after entering a number
+                String strDesiredDistance = desiredDistance.getText().toString();
+                desiredDistance.setSelection(desiredDistance.getText().length());
+                if (strDesiredDistance.matches("")) {
+                    //does nothing, this should work, double check please
+                }else {
+                    seekBarDistance.setProgress(Integer.parseInt(editable.toString()));
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+        });
     }
 }
